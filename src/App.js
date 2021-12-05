0ccfc87f1bca671536d7dea359df077a71674ad3
Andrei Cirlan <andreynho2006@gmail.com>
2021-12-05 18:33:28 +0200

    update functions for start, stop and reset timer

--- src/App.js
+++ src/App.js
@@ -12,20 +12,43 @@ function padTime(time) {
 export default function App() {
   const [timeLeft, setTimeLeft] = useState(25 * 60)
   const [title, setTitle] = useState('Let the countdown begin!!!')
+  const [isRunning, setIsRunning] = useState(false);
   const intervalRef = useRef(null);
 
   function startTimer() {
+    if (intervalRef.current != null) return;
+
+    setTitle(`You're doing great!`)
+    setIsRunning(true);
     intervalRef.current = setInterval(() => {
       setTimeLeft(timeLeft => {
         if(timeLeft >= 1) return timeLeft - 1
         
+        // reset the timer
+        resetTimer()
         return 0;
       });
     }, 1000)
   }
 
   function stopTimer() {
+    if (intervalRef.current == null) return;
+    
     clearInterval(intervalRef.current);
+    intervalRef.current = null;
+    setTitle('Keep it up!!!')
+    setIsRunning(false);
+  }
+
+
+  function resetTimer() {
+
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+   
+    setTitle('Ready to go another round!!!')
+    setTimeLeft(25*60)
+    setIsRunning(false);
   }
 
   const minutes = padTime(Math.floor(timeLeft / 60))
@@ -42,9 +65,9 @@ export default function App() {
       </div>
 
       <div className="buttons">
-        <button onClick={startTimer}>Start</button>
-        <button onClick={stopTimer}>Stop</button>
-        <button>Reset</button>
+        { !isRunning && <button onClick={startTimer}>Start</button>}
+        { isRunning && <button onClick={stopTimer}>Stop</button>}
+        <button onClick={resetTimer}>Reset</button>
       </div>
     </div>
   );
