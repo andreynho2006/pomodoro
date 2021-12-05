d23a21b0237b2dbf061feb3e9aa200cd086f857d
Andrei Cirlan <andreynho2006@gmail.com>
2021-12-05 16:50:08 +0200

    make function to pad time, add function to start a timer with setInterval

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import React, { useState } from 'react';
 import './App.css';
 
-export default function App() {
+function padTime(time) {
+  return time.toString().padStart(2, '0')
+}
 
+export default function App() {
   const [timeLeft, setTimeLeft] = useState(25 * 60)
-  const minutes = Math.floor(timeLeft / 60).toString().padStart(2, '0')
-  const seconds = (timeLeft - minutes * 60).toString().padStart(2, '0')
+  const [title, setTitle] = useState('Let the countdown begin!!!')
+
+  function startTimer() {
+    setInterval(() => {
+      setTimeLeft(timeLeft => {
+        if(timeLeft >= 1) return timeLeft - 1
+        
+        // reset timer
+        return 0;
+      });
+    }, 1000)
+  }
+
+  const minutes = padTime(Math.floor(timeLeft / 60))
+  const seconds = padTime(timeLeft - minutes * 60)
+
   return (
     <div className="app">
-      <h2>Pomodoro!</h2>
+      <h2>{title}</h2>
 
       <div className="timer">
-        <span>{ minutes }</span>
+        <span>{minutes}</span>
         <span>:</span>
-        <span>{ seconds }</span>
+        <span>{seconds}</span>
       </div>
 
       <div className="buttons">
-        <button>Start</button>
+        <button onClick={startTimer}>Start</button>
         <button>Stop</button>
         <button>Reset</button>
       </div>
